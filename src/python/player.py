b0d9fa300df7afa3c20a20505f311d8e99eee593
Chris Anzilotti <anzilottichr@gmail.com>
2024-11-09 12:11:50 -0500

    commit

--- src/python/player.py
+++ src/python/player.py
@@ -1,7 +1,6 @@
-# Player class
 import pygame
 
-from constants import GREEN
+from python.constants import GREEN
 
 
 class Player(pygame.sprite.Sprite):
@@ -11,10 +10,14 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(GREEN)
         self.rect = self.image.get_rect(topleft=(x, y))
         self.velocity_y = 0
+        self.velocity_x = 0
         self.on_ground = False
+        self.on_wall = False
         self.speed = 5
         self.jump_height = -15
+        self.jump_direction_strength = 8  # How strong the jump is when moving diagonally
         self.gravity = 1
+        self.slide_speed = 2  # Speed at which the player slides down the wall
 
         # Dash attributes
         self.dash_speed = 20  # Speed of the dash
@@ -28,7 +31,6 @@ class Player(pygame.sprite.Sprite):
         self.key_press = {}
 
     def update(self, platforms, walls):
-        # Horizontal movement
         # Get pressed keys
         keys = pygame.key.get_pressed()
 
@@ -38,11 +40,12 @@ class Player(pygame.sprite.Sprite):
             elif event.type == pygame.KEYUP:
                 self.key_press[event.key] = False
 
-        # Normal movement
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+        # Normal movement (not dashing)
+        if not self.dashing:
+            if keys[pygame.K_LEFT]:
+                self.rect.x -= self.speed
+            if keys[pygame.K_RIGHT]:
+                self.rect.x += self.speed
 
         # Initiate dash if the shift key is pressed and cooldown allows
         if (keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]) and self.cooldown_timer <= 0:
@@ -60,17 +63,32 @@ class Player(pygame.sprite.Sprite):
             if self.dash_timer > 0:
                 if self.dash_direction == "left":
                     self.rect.x -= self.dash_speed
+                    for wall in walls:
+                        if self.rect.colliderect(wall.rect):
+                            self.rect.left = wall.rect.right
+                            self.on_wall = True
+                            break
                 elif self.dash_direction == "right":
                     self.rect.x += self.dash_speed
-                    self.dash_timer -= 1
+                    for wall in walls:
+                        if self.rect.colliderect(wall.rect):
+                            self.rect.right = wall.rect.left
+                            self.on_wall = True
+                            break
+                self.dash_timer -= 1
             else:
                 self.dashing = False
                 self.invincible = False  # Stop being invincible after the dash
 
-
-        # Apply gravity
-        self.velocity_y += self.gravity
-        self.rect.y += self.velocity_y
+        # Apply gravity if not sliding
+        if not self.on_wall:
+            self.velocity_y += self.gravity
+            self.rect.y += self.velocity_y
+        else:
+            # Apply sliding gravity when touching wall, but only after falling (velocity_y > 0)
+            if self.velocity_y > 0:
+                self.velocity_y += self.slide_speed
+                self.rect.y += self.velocity_y
 
         # Check for collision with platforms
         self.on_ground = False
@@ -79,15 +97,46 @@ class Player(pygame.sprite.Sprite):
                 self.rect.bottom = platform.rect.top
                 self.velocity_y = 0
                 self.on_ground = True
-        # check for collision with walls and disallow it
-        for wall in walls:
-            if self.rect.colliderect(wall.rect):
-                self.rect.left = wall.rect.right
-                self.velocity_x = 0
 
         # Jump if on ground
         if keys[pygame.K_SPACE] and self.on_ground:
             self.velocity_y = self.jump_height
+
+        # Wall jump handling
+        if self.on_wall and keys[pygame.K_SPACE]:
+            # Apply jump velocity upwards and to the right or left, depending on wall side
+            if keys[pygame.K_LEFT]:
+                self.velocity_y = self.jump_height
+                self.velocity_x = self.jump_direction_strength  # Move to the left
+            elif keys[pygame.K_RIGHT]:
+                self.velocity_y = self.jump_height
+                self.velocity_x = -self.jump_direction_strength  # Move to the right
+            self.on_wall = False  # Stop wall sliding once you jump
+
+        # Check for collision with walls
+        self.on_wall = False
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                # Detect collision with wall and enable sliding if not grounded and falling (velocity_y > 0)
+                if not self.on_ground and self.velocity_y > 0:
+                    self.on_wall = True
+                    self.velocity_y = self.slide_speed  # Begin sliding down
+
+                    # Optional: allow horizontal movement while sliding
+                    if keys[pygame.K_LEFT]:
+                        self.rect.x -= self.speed
+                    if keys[pygame.K_RIGHT]:
+                        self.rect.x += self.speed
+
+                # Prevent the player from going through the wall
+                if self.rect.left < wall.rect.right:
+                    self.rect.left = wall.rect.right
+                elif self.rect.right > wall.rect.left:
+                    self.rect.right = wall.rect.left
+
+        # Prevent jumping off the wall if on the ground
+        if self.on_ground:
+            self.on_wall = False
 
     def start_dash(self, direction):
         """Initiate a dash in the specified direction."""
