c72ad2c1d76181b74eac833630a7cb1cb7715cb8
PeterZegarek <petrzegarek@gmail.com>
2024-11-09 10:46:13 -0500

    SIdescroler

---
+++ constants.py
@@ -0,0 +1,6 @@
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+# Colors
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
---
+++ src/platforms.py
@@ -0,0 +1,11 @@
+# Platform class
+import pygame
+
+from constants import BLUE
+
+class Platforms(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect(topleft=(x, y))
